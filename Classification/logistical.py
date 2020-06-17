0c11d8023d09a1edcf8b3ffddadecfb11b250ad4
racec9999 <racec9999@gmail.com>
2020-06-16 21:41:09 -0500

    Add classification

---
+++ Classification/logistical.py
@@ -0,0 +1,88 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dt = pd.read_csv('Social_Network_Ads.csv')
+
+
+X = dt.iloc[:,[2,3]].values
+Y = dt.iloc[:,4].values
+
+
+
+
+# split the dataset in training and testing
+
+from sklearn.model_selection import train_test_split
+X_train ,X_test ,Y_train, Y_test = train_test_split(X, Y, test_size = 0.25,random_state = 0)
+
+
+#e Scale the variables
+
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+
+#adjust the logistic regression model in the training set
+
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0,solver = 'liblinear')
+classifier.fit(X_train, Y_train)
+
+
+#prediction
+y_pred = classifier.predict(X_test)
+
+
+# confsion matrix
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(Y_test,y_pred)
+
+
+
+
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, Y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Clasificador (Conjunto de Entrenamiento)')
+plt.xlabel('Edad')
+plt.ylabel('Sueldo Estimado')
+plt.legend()
+plt.show()
+
+
+# Representación gráfica de los resultados del algoritmo en el Conjunto de Testing
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Clasificador (Conjunto de Test)')
+plt.xlabel('Edad')
+plt.ylabel('Sueldo Estimado')
+plt.legend()
+plt.show()
+
+
+
+
+
+
+
+
