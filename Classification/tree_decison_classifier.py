faa956b9d4924bec22ca750898600ec04e4ec336
Cesar Arcos <60860385+racec9999@users.noreply.github.com>
2020-11-04 12:06:26 -0600

    Update tree_decison_classifier.py

--- Classification/tree_decison_classifier.py
+++ Classification/tree_decison_classifier.py
@@ -69,7 +69,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Árbol de Decisión (Test set)')
+plt.title('Decision tree (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
