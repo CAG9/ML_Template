40fa200cc3d8cfef3e37e782b57ca0ba00d80852
Cesar Arcos <60860385+racec9999@users.noreply.github.com>
2020-11-05 09:35:59 -0600

    Update lda.py

--- Dimensional_Reduction/lda.py
+++ Dimensional_Reduction/lda.py
@@ -4,55 +4,54 @@
 """
 Created on Sat Apr 11 21:19:21 2020
 
-@author: cesar
+@author: César Arcos
 """
 
 # LDA
 
-# Cómo importar las librerías
+# Import libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importar el data set
+# Import dataset
 dataset = pd.read_csv('Wine.csv')
 
 X = dataset.iloc[:, 0:13].values
 y = dataset.iloc[:, 13].values
 
-
-# Dividir el data set en conjunto de entrenamiento y conjunto de testing
+# Split the dataset into training and testing set 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 
-# Escalado de variables
+# Scale the data
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 
-
-# Reducir la dimensión del dataset con LDA
+# Reduce dimension using LDA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 lda = LDA(n_components = 2)
 X_train = lda.fit_transform(X_train, y_train)
 X_test = lda.transform(X_test)
 
 
-# Ajustar el modelo de Regresión Logística en el Conjunto de Entrenamiento
+# Train the classifier
+
 from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression(random_state = 0)
 classifier.fit(X_train, y_train)
 
-# Predicción de los resultados con el Conjunto de Testing
+# Predict
 y_pred  = classifier.predict(X_test)
 
-# Elaborar una matriz de confusión
+# Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 
-# Representación gráfica de los resultados del algoritmo en el Conjunto de Entrenamiento
+# Graphic representation of the results in training set 
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_train, y_train
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
@@ -64,14 +63,14 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green', 'blue'))(i), label = j)
-plt.title('Clasificador (Conjunto de Entrenamiento)')
+plt.title('Classifier (Training set)')
 plt.xlabel('DL1')
 plt.ylabel('DL2')
 plt.legend()
 plt.show()
 
 
-# Representación gráfica de los resultados del algoritmo en el Conjunto de Testing
+# Graphic representation of the results in testing set 
 X_set, y_set = X_test, y_test
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
@@ -82,7 +81,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green', 'blue'))(i), label = j)
-plt.title('Clasificador (Conjunto de Test)')
+plt.title('Classifier (Test set)')
 plt.xlabel('DL1')
 plt.ylabel('DL2')
 plt.legend()
