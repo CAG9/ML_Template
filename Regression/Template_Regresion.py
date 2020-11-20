7c5f59dfcfe98f90eac5635b5965430b7ab43013
Cesar Arcos <60860385+racec9999@users.noreply.github.com>
2020-11-19 20:42:52 -0600

    Update Template_Regresion.py

--- Regression/Template_Regresion.py
+++ Regression/Template_Regresion.py
@@ -6,42 +6,29 @@ Created on Fri Mar 20 22:03:29 2020
 @author: Cesar Arcos GOnzalez
 
 """
-
-
+# Import libraries
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
-
+# Import the dataset
 df = pd.read_csv('Position_Salaries.csv')
-
-
 X = df.iloc[:,1:2].values
 Y = df.iloc[:,2:3].values
-
-
-"""
+# Split the dataset into training and testing set 
 from sklearn.model_selection import train_test_split
 X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size = 0.2,random_state = 0)
-"""
-
-"""
+# Scale the data
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
-"""
-
-# Adjust the dataset with lenear regression 
-
-
-
-
-#predict
+# Train 
+from sklearn.ensemble import RandomForestRegressor
+regression = RandomForestRegressor(n_estimators = 300, random_state = 0)
+regression.fit(X,Y)
+# Predict
 y_pred = regression.predict() 
-
-
-#visualizate
+# Graphic representation of the results 
 X_grid = np.arange(min(X), max(X), 0.1) # to adjust the curve with more points
 X_grid = X_grid.reshape(len(X_grid), 1) # convert the vector into a matrix
 plt.scatter(X, Y, color = "red")
@@ -50,9 +37,3 @@ plt.title("Polynomial Regression Model")
 plt.xlabel("Employee position")
 plt.ylabel("Salary (USD dollar)")
 plt.show()
-
-
-
-
-
-
