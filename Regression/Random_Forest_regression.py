014f923046cd0a7378b172031ebfcde717e7a193
Cesar Arcos <60860385+racec9999@users.noreply.github.com>
2020-11-19 20:32:59 -0600

    Update Random_Forest_regression.py

--- Regression/Random_Forest_regression.py
+++ Regression/Random_Forest_regression.py
@@ -3,28 +3,23 @@
 """
 Created on Sun Mar 22 10:05:51 2020
 
-@author: cesar
+@author: Cesar Arcos
 """
-
+#Import libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
-# Importar el data set
+# Import the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 Y = dataset.iloc[:, 2].values
-
-
-
+# Train the classifier
 from sklearn.ensemble import RandomForestRegressor
 regression = RandomForestRegressor(n_estimators = 300, random_state = 0)
 regression.fit(X,Y)
-
-
+# Predict
 y_pred = regression.predict(6.5)
-
-# Visualización de los resultados del Modelo Polinómico
+# Graphic representation of the results 
 X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape(len(X_grid), 1)
 plt.scatter(X, y, color = "red")
