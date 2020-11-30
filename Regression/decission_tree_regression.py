f900a632c89db19bea1d2fbe715dbe3a2bc70188
Cesar Arcos <60860385+racec9999@users.noreply.github.com>
2020-11-29 21:21:53 -0600

    Update decission_tree_regression.py

--- Regression/decission_tree_regression.py
+++ Regression/decission_tree_regression.py
@@ -7,45 +7,44 @@ Created on Sat Mar 21 18:34:04 2020
 """
 
 
-# Regresión con Árboles de Decisión
+# Decission Tree Regression 
 
-# Cómo importar las librerías
+#Import libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importar el data set
+# Import the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
 
-# Dividir el data set en conjunto de entrenamiento y conjunto de testing
+# Split the dataset into training and testing set 
 """
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 """
-
-# Escalado de variables
+# Scale the data
 """from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)"""
 
-# Ajustar la regresión con el dataset
+# Train the model 
 from sklearn.tree import DecisionTreeRegressor
 regression = DecisionTreeRegressor(random_state = 0)
 regression.fit(X, y)
 
-# Predicción de nuestros modelos
+## Predict 
 y_pred = regression.predict(6.5)
 
-# Visualización de los resultados del Modelo Polinómico
+# Graphic representation of the results
 X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape(len(X_grid), 1)
 plt.scatter(X, y, color = "red")
 plt.plot(X, regression.predict(X), color = "blue")
-plt.title("Modelo de Regresión")
-plt.xlabel("Posición del empleado")
-plt.ylabel("Sueldo (en $)")
+plt.title("Tree Regression Model")
+plt.xlabel("Employee position")
+plt.ylabel("Salary (USD dollar)")
 plt.show()
