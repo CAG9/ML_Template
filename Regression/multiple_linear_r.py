cbcf760c7be63e8fed3fdac8f2733fd35ab7558f
Cesar Arcos <60860385+racec9999@users.noreply.github.com>
2020-12-02 11:25:24 -0600

    Update multiple_linear_r.py

--- Regression/multiple_linear_r.py
+++ Regression/multiple_linear_r.py
@@ -1,49 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
-# Importar el data set
+# Import the dataset
 dataset = pd.read_csv('50_Startups.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 4].values
-
-
-# Codificar datos categóricos
+# Encode the variables
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
 X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
 onehotencoder = OneHotEncoder(categorical_features=[3])
 X = onehotencoder.fit_transform(X).toarray()
-
-# Evitar la trampa de las variables ficticias
+#Avoid the trap of fictitious variables
 X = X[:, 1:]
-
-# Dividir el data set en conjunto de entrenamiento y conjunto de testing
+# split the dataset in training and testing
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-
-
-# Escalado de variables
+# Scale the variables
 """from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)"""
-
-# Ajustar el modelo de Regresión lineal múltiple con el conjunto de entrenamiento
+# Train the classifier
 from sklearn.linear_model import LinearRegression
 regression = LinearRegression()
 regression.fit(X_train, y_train)
-
-# Predicción de los resultados en el conjunto de testing
+# Predict
 y_pred = regression.predict(X_test)
-
-# Construir el modelo óptimo de RLM utilizando la Eliminación hacia atrás
+# Build the optimal RLM model using backward elimination
 import statsmodels.formula.api as sm
 X = np.append(arr = np.ones((50,1)).astype(int), values = X, axis = 1)
 SL = 0.05
-
-#Se ha añadido el modificador .tolist() al X_opt para adaptarse a Python 3.7
-
+# Has been added the modifier .tolist() to X_opt for Python 3.7
 X_opt = X[:, [0, 1, 2, 3, 4, 5]]
 regression_OLS = sm.OLS(endog = y, exog = X_opt.tolist()).fit()
 regression_OLS.summary()
