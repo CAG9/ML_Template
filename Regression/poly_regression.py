ef17127ae40d06517656ccf4c30c77998998bb1b
Cesar Arcos <60860385+racec9999@users.noreply.github.com>
2020-12-06 16:42:17 -0600

    Update poly_regression.py

--- Regression/poly_regression.py
+++ Regression/poly_regression.py
@@ -4,28 +4,20 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
-
+# Import the dataset
 df = pd.read_csv('Position_Salaries.csv')
-
-
 X = df.iloc[:,1:2].values
 Y = df.iloc[:,2:3].values
-
-# Adjust the dataset with lenear regression 
+# Train with linear regression 
 from sklearn.linear_model import LinearRegression
 lin_reg = LinearRegression()
 lin_reg.fit(X,Y)
-
-
-
-#adjust the daset with polynomial regression
+# Train with polynomial regression
 from sklearn.preprocessing import PolynomialFeatures
 poly_reg = PolynomialFeatures(degree = 4)
-X_poly = poly_reg.fit_transform(X) # create a "dataset" with diferen degrees
+X_poly = poly_reg.fit_transform(X) # create a "dataset" with diferent degrees
 lin_reg_2 = LinearRegression()
 lin_reg_2.fit(X_poly,Y)
-
 #Visualize the result with Polynomical Regression
 plt.scatter(X, Y, color = "red")
 plt.plot(X, lin_reg.predict(X), color = "blue")
@@ -33,7 +25,6 @@ plt.title("Linear Regression Model")
 plt.xlabel("Employee position")
 plt.ylabel("Salary (USD dollar)")
 plt.show()
-
 #Visualize the result with Polynomical Regression
 X_grid = np.arange(min(X), max(X), 0.1) # to adjust the curve with more points
 X_grid = X_grid.reshape(len(X_grid), 1) # convert the vector into a matrix
@@ -43,11 +34,5 @@ plt.title("Polynomial Regression Model")
 plt.xlabel("Employee position")
 plt.ylabel("Salary (USD dollar)")
 plt.show()
-
-#predicting the salary of an employee
+# Predicting the salary of an employee
 lin_reg_2.predict(poly_reg.fit_transform([[6.5]]))
-
-
-
-
-
