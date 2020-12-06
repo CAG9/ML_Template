08e7f1018b06929ce69fc9bed9a3b4c5d86b9f74
Cesar Arcos <60860385+racec9999@users.noreply.github.com>
2020-12-06 17:53:50 -0600

    Update Boost.py

--- Xgboost/Boost.py
+++ Xgboost/Boost.py
@@ -5,24 +5,16 @@ Created on Sun Apr 12 11:49:42 2020
 
 @author: cesar
 """
-
-
-
 # XGBoost
-# Las instrucciones de instalación se pueden consultar en http://xgboost.readthedocs.io/en/latest/build.html
-
-# Cómo importar las librerías
+# You may watch the instalation instruction here : http://xgboost.readthedocs.io/en/latest/build.html
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
-# Importar el data set
+# Import the dataset
 dataset = pd.read_csv('Churn_Modelling.csv')
-
 X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
-
-# Codificar datos categóricos
+# Encode categorial data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X_1 = LabelEncoder()
 X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
@@ -31,24 +23,19 @@ X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
 onehotencoder = OneHotEncoder(categorical_features=[1])
 X = onehotencoder.fit_transform(X).toarray()
 X = X[:, 1:]
-
-# Dividir el data set en conjunto de entrenamiento y conjunto de testing
+# split the dataset in training and testing
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-
-# Ajustar el modelo XGBoost al Conjunto de Entrenamiento
+# Train
 from xgboost import XGBClassifier
 classifier = XGBClassifier()
 classifier.fit(X_train, y_train)
-
-# Predicción de los resultados con el Conjunto de Testing
+# Predict
 y_pred  = classifier.predict(X_test)
-
-# Elaborar una matriz de confusión
+# Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
-
-# Aplicar k-fold cross validation
+# Apply k-fold 
 from sklearn.model_selection import cross_val_score
 accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
 accuracies.mean()
