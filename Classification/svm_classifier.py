89738db46ca9cf477124c500597efc56e8bf5dba
Cesar Arcos <60860385+racec9999@users.noreply.github.com>
2020-12-31 23:32:46 -0600

    Update svm_classifier.py

--- Classification/svm_classifier.py
+++ Classification/svm_classifier.py
@@ -2,49 +2,33 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Mar 24 21:54:17 2020
-
 @author: Cesar Arcos
+Contact info: cesar99ag@gmail.com
 """
-
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
 # Import the dataset
 dataset = pd.read_csv('Social_Network_Ads.csv')
-
 X = dataset.iloc[:, [2,3]].values
 y = dataset.iloc[:, 4].values
-
 # Split the dataset into training and testing set 
-
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
-
-
 # Scale the data
-
-
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
-
-
 # Adjust SVM 
 from sklearn.svm import SVC
 classifier = SVC(kernel = "linear",random_state = 0)
 classifier.fit(X_train,y_train)
-
 # Predict 
 y_pred = classifier.predict(X_test)
-
 # Confusion matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test,y_pred)
-
-
-
 # Graphic representation of the results in training set 
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_train, y_train
@@ -62,8 +46,6 @@ plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
 plt.show()
-
-
 # Graphic representation of the results in testing set 
 X_set, y_set = X_test, y_test
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
