6725a4170c58b8ee341a16b750840523b117659e
Cesar Arcos <60860385+racec9999@users.noreply.github.com>
2020-12-31 23:34:31 -0600

    Update hc.py

--- Clustering/hc.py
+++ Clustering/hc.py
@@ -2,21 +2,17 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Apr  6 15:53:31 2020
-
 @author: Cesar Arcos
+Contact info: cesar99ag@gmail.com
 """
-
 # Hierarchical clustering
-
 # Import the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
 # Import the dataset
 dataset = pd.read_csv("Mall_Customers.csv")
 X = dataset.iloc[:, [3, 4]].values
-
 # Using dendrogram to find the optimal number of clusters
 import scipy.cluster.hierarchy as sch
 dendrogram = sch.dendrogram(sch.linkage(X, method = "ward"))
@@ -24,13 +20,10 @@ plt.title("Dendrogram")
 plt.xlabel("Clients")
 plt.ylabel("Euclidean distances")
 plt.show()
-
 # Fit the cluster
 from sklearn.cluster import AgglomerativeClustering
 hc = AgglomerativeClustering(n_clusters = 5, affinity = "euclidean", linkage = "ward")
 y_hc = hc.fit_predict(X)
-
-
 # Visualizing the clusters
 plt.scatter(X[y_hc == 0, 0], X[y_hc == 0, 1], s = 100, c = "red", label = "Cautious")
 plt.scatter(X[y_hc == 1, 0], X[y_hc == 1, 1], s = 100, c = "blue", label = "Standard")
