5aef1e4d22c4b7542f58e737ff2a23980c74bff3
Cesar Arcos <60860385+racec9999@users.noreply.github.com>
2020-12-31 23:37:04 -0600

    Update lda.py

--- Dimensional_Reduction/lda.py
+++ Dimensional_Reduction/lda.py
@@ -3,54 +3,39 @@
 #%%
 """
 Created on Sat Apr 11 21:19:21 2020
-
 @author: César Arcos
+Contact info: cesar99ag@gmail.com
 """
-
 # LDA
-
-# Import libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
 # Import dataset
 dataset = pd.read_csv('Wine.csv')
-
 X = dataset.iloc[:, 0:13].values
 y = dataset.iloc[:, 13].values
-
 # Split the dataset into training and testing set 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-
-
 # Scale the data
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
-
 # Reduce dimension using LDA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 lda = LDA(n_components = 2)
 X_train = lda.fit_transform(X_train, y_train)
 X_test = lda.transform(X_test)
-
-
 # Train the classifier
-
 from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression(random_state = 0)
 classifier.fit(X_train, y_train)
-
 # Predict
 y_pred  = classifier.predict(X_test)
-
 # Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
-
 # Graphic representation of the results in training set 
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_train, y_train
@@ -68,8 +53,6 @@ plt.xlabel('DL1')
 plt.ylabel('DL2')
 plt.legend()
 plt.show()
-
-
 # Graphic representation of the results in testing set 
 X_set, y_set = X_test, y_test
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
@@ -86,10 +69,3 @@ plt.xlabel('DL1')
 plt.ylabel('DL2')
 plt.legend()
 plt.show()
-
-
-
-
-
-
-
