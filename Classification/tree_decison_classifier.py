b79122000b17dbfc8efdef03922e29c665e0f0d6
Cesar Arcos <60860385+racec9999@users.noreply.github.com>
2020-12-31 23:34:00 -0600

    Update tree_decison_classifier.py

--- Classification/tree_decison_classifier.py
+++ Classification/tree_decison_classifier.py
@@ -2,43 +2,29 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Mar 25 21:15:07 2020
-
 @author: Cesar Arcos
+Contact info: cesar99ag@gmail.com
 """
-
 # Decision Tree classifier
-
-
-# Import the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
-# Importar el data set
+# Import dataset
 dataset = pd.read_csv('Social_Network_Ads.csv')
-
 X = dataset.iloc[:, [2,3]].values
 y = dataset.iloc[:, 4].values
-
-
 # Split the dataset into training and testing set 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
-
-
 # Train the classifier
 from sklearn.tree import DecisionTreeClassifier
 classifier = DecisionTreeClassifier(criterion = "entropy", random_state = 0)
 classifier.fit(X_train, y_train)
-
-
 # Predict
 y_pred  = classifier.predict(X_test)
-
 # Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
-
 # Graphic representation of the results in training set 
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_train, y_train
@@ -56,8 +42,6 @@ plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
 plt.show()
-
-
 # Graphic representation of the results in testing set 
 X_set, y_set = X_test, y_test
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 1),
