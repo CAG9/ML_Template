e385c78f94fc4692dec357e4f4feccaf3707705e
Cesar Arcos <60860385+racec9999@users.noreply.github.com>
2020-12-31 23:30:46 -0600

    Update logistical.py

--- Classification/logistical.py
+++ Classification/logistical.py
@@ -1,49 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  27 12:41:33 2020
+@author: Cesar Arcos
+Contact info: cesar99ag@gmail.com
+"""
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
+# Import Dataset
 dt = pd.read_csv('Social_Network_Ads.csv')
-
-
 X = dt.iloc[:,[2,3]].values
 Y = dt.iloc[:,4].values
-
-
-
-
 # split the dataset in training and testing
-
 from sklearn.model_selection import train_test_split
 X_train ,X_test ,Y_train, Y_test = train_test_split(X, Y, test_size = 0.25,random_state = 0)
-
-
-#e Scale the variables
-
+# Scale the variables
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
-
-
-#adjust the logistic regression model in the training set
-
+# Adjust the logistic regression model in the training set
 from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression(random_state = 0,solver = 'liblinear')
 classifier.fit(X_train, Y_train)
-
-
-#prediction
+# Prediction
 y_pred = classifier.predict(X_test)
-
-
-# confusion matrix
-
+# Confusion matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(Y_test,y_pred)
-
-
-
-
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_train, Y_train
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
@@ -60,8 +45,6 @@ plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
 plt.show()
-
-
 # Graphic representation of the results in testing set 
 X_set, y_set = X_test, y_test
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
@@ -78,11 +61,3 @@ plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
 plt.show()
-
-
-
-
-
-
-
-
