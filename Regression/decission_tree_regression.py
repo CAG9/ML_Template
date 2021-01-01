cfa71e9f340bb10925e0470fba87bdb2c33a3a84
Cesar Arcos <60860385+racec9999@users.noreply.github.com>
2020-12-31 23:43:21 -0600

    Update decission_tree_regression.py

--- Regression/decission_tree_regression.py
+++ Regression/decission_tree_regression.py
@@ -2,24 +2,17 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Mar 21 18:34:04 2020
-
-@author: cesar
+@author: Cesar Arcos
+Contact info: cesar99ag@gmail.com
 """
-
-
 # Decission Tree Regression 
-
-#Import libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
 # Import the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
-
-
 # Split the dataset into training and testing set 
 """
 from sklearn.model_selection import train_test_split
@@ -30,15 +23,12 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)"""
-
 # Train the model 
 from sklearn.tree import DecisionTreeRegressor
 regression = DecisionTreeRegressor(random_state = 0)
 regression.fit(X, y)
-
 ## Predict 
 y_pred = regression.predict(6.5)
-
 # Graphic representation of the results
 X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape(len(X_grid), 1)
